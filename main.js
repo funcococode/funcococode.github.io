003c00327bfc0fda017dd5b9a8f91e6133d1e8c9
funcode <funcococode@gmail.com>
2020-12-31 14:50:36 +0530

    Added responsiveness and inputbox for mobile devices

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const keyup = document.querySelector("#keyup");
 const keypress = document.querySelector("#keypress");
 const keydown = document.querySelector("#keydown");
+const keyInput = document.querySelector("#keyInput")
 
 const infoChar = document.querySelector("#character");
 const infoKeyup = document.querySelector("#infoKeyup");
@@ -28,6 +29,39 @@ addEventListener("keyup", (e) => {
                     `
 })
 addEventListener("keydown", (e) => {
+
+    keydown.innerHTML = `${e.keyCode}`;
+    infoKeydown.innerHTML = `
+                    <br>Shift Key : <span style="color:${(e.shiftKey)?'green':'red'};text-transform:capitalize"> ${e.shiftKey} </span>
+                    <br>Ctrl Key : <span style="color:${(e.ctrlKey)?'green':'red'};text-transform:capitalize">${e.ctrlKey}</span>
+                    <br>Alt Key : <span style="color:${(e.altKey)?'green':'red'};text-transform:capitalize">${e.altKey}</span>
+                    <br>Meta Key : <span style="color:${(e.metaKey)?'green':'red'};text-transform:capitalize">${e.metaKey}</span>
+                    `
+})
+
+
+
+keyInput.addEventListener("keypress", (e) => {
+    keypress.innerHTML = `${e.keyCode}`;
+    infoChar.innerHTML = `Character : ${e.code}`;
+    infoKeypress.innerHTML = `
+                    <br>Shift Key : <span style="color:${(e.shiftKey)?'green':'red'};text-transform:capitalize"> ${e.shiftKey} </span>
+                    <br>Ctrl Key : <span style="color:${(e.ctrlKey)?'green':'red'};text-transform:capitalize">${e.ctrlKey}</span>
+                    <br>Alt Key : <span style="color:${(e.altKey)?'green':'red'};text-transform:capitalize">${e.altKey}</span>
+                    <br>Meta Key : <span style="color:${(e.metaKey)?'green':'red'};text-transform:capitalize">${e.metaKey}</span>
+                    `
+})
+
+keyInput.addEventListener("keyup", (e) => {
+    keyup.innerHTML = `${e.keyCode}`;
+    infoKeyup.innerHTML = `
+                    <br>Shift Key : <span style="color:${(e.shiftKey)?'green':'red'};text-transform:capitalize"> ${e.shiftKey} </span>
+                    <br>Ctrl Key : <span style="color:${(e.ctrlKey)?'green':'red'};text-transform:capitalize">${e.ctrlKey}</span>
+                    <br>Alt Key : <span style="color:${(e.altKey)?'green':'red'};text-transform:capitalize">${e.altKey}</span>
+                    <br>Meta Key : <span style="color:${(e.metaKey)?'green':'red'};text-transform:capitalize">${e.metaKey}</span>
+                    `
+})
+keyInput.addEventListener("keydown", (e) => {
 
     keydown.innerHTML = `${e.keyCode}`;
     infoKeydown.innerHTML = `
