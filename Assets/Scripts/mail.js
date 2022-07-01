f8e21035ac69bd8cb325c667b46266c5d660155a
Rachit Shrivastava <rachitshrivastava@Rachits-MacBook-Pro.local>
2022-07-02 01:24:34 +0530

    changes in mail body structure

--- Assets/Scripts/mail.js
+++ Assets/Scripts/mail.js
@@ -15,9 +15,13 @@ const sendEmail = (event) => {
         message: message.value
     };
     
-    let body = `Name : <b>${data.name}<b><br/>
-    Email : <b>${data.email}</b><br/>
-    ${data.message}`;
+    let mailSubject = `${data.name} - ${data.subject}`;
+    let body = `
+                    Name : <b>${data.name}</b><br/>
+                    Subject : <b>${data.subject}</b><br/>
+                    Email : <b>${data.email}</b><br/><br/>
+                    ${data.message}
+                `;
 
     sendBtn.classList.add('bg-yellow-500', 'text-white', 'hover:bg-yellow-600');
     sendBtn.innerHTML = 'On The Way! <i class="spinner fa-solid fa-spinner"></i>';
@@ -31,7 +35,7 @@ const sendEmail = (event) => {
             Password : "0E7EC3A1D8323E841DB9B3A3A193574A64D8",
             To : 'connect@studiohr.in',
             From : "query.studiohr@gmail.com",
-            Subject : data.subject,
+            Subject : mailSubject,
             Body : body,
         }).then(
             cfs.innerHTML = `<p class="lg:px-64 md:px-20 px-8 bg-gray-100 mt-10 rounded-xl text-gray-300 font-black lg:text-7xl text-5xl  py-24 lg:leading-tight leading-tight text-center">
